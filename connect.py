cc38c87e71effe41ecc4df4f6963f06802914b21
PinsaraPerera <1pawanpinsara@gmail.com>
2022-10-22 00:20:27 +0530

    updated the sql query with if not exists..

--- connect.py
+++ connect.py
@@ -8,7 +8,7 @@ db = mysql.connector.connect(
 )
 
 Q1 = "CREATE TABLE if not exists Users(user_name VARCHAR(50) PRIMARY KEY NOT NULL," \
-     " email VARCHAR(50) NOT NULL,password VARCHAR(8) NOT NULL, created datetime NOT NULL) "
+     " email VARCHAR(50) NOT NULL,password VARCHAR(8) NOT NULL, created datetime NOT NULL)"
 
 mycursor = db.cursor()
 mycursor.execute(Q1)
